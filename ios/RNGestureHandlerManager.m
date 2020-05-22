09be4463e990ccf21d8c5f00b34130947601da28
Vishal chaturvedi <vc121356@gmail.com>
2020-05-22 21:03:15 +0530

    fixed .61 issue

--- ios/RNGestureHandlerManager.m
+++ ios/RNGestureHandlerManager.m
@@ -5,7 +5,9 @@
 #import <React/RCTComponent.h>
 #import <React/RCTRootView.h>
 #import <React/RCTTouchHandler.h>
+#if __has_include(<React/RCTRootContentView.h>)
 #import <React/RCTRootContentView.h>
+#endif
 
 #import "RNGestureHandlerState.h"
 #import "RNGestureHandler.h"
@@ -160,7 +162,11 @@
 
     UIView *parent = rootContentView.superview;
     if ([parent isKindOfClass:[RCTRootView class]]) {
-        [((RCTRootContentView*)rootContentView).touchHandler cancel];
+        #if __has_include(<React/RCTRootContentView.h>)
+                [((RCTRootContentView*)rootContentView).touchHandler cancel];
+        #else
+                [(RCTRootView*)parent cancelTouches];
+        #endif
     }
 }
 
